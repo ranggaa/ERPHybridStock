16564eb8275501da0a8f2345df24d89c70cca3c2
lalatendu11 <lalatendung@gmail.com>
2024-05-27 12:12:22 +0530

    New Class with methods

---
+++ Hybrid_FrameWork/src/test/java/commonFunctions/Demo.java
@@ -0,0 +1,25 @@
+package commonFunctions;
+
+public class Demo {
+public void add()
+{
+	int a=98;
+	int b=8;
+	int c=a+b;
+	System.out.println(c);
+}
+public void sub()
+{
+	int a=5677;
+	int b=5;
+	int c=a+b;
+	System.out.println(c);
+}
+public void div()
+{
+	int a=3456;
+	int b=89;
+	int c=a+b;
+	System.out.println(c);
+}
+}
